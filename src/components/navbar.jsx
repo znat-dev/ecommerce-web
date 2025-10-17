b1ad2dedf7f3e96ca42e11cfbfd14370bc738335
znat-dev <natnaelbekele789@gmail.com>
2025-10-17 05:12:59 +0300

    Changed so features and added css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ShoppingCart } from 'phosphor-react';
+import "../App.css";
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className="navbar">
+      <h1 className='megenagna' >Megenagna Online Shopping Market</h1>
+        <div className="links">
             <Link to="/">Shop</Link>
             <Link to="cart">
                 <ShoppingCart size={32} />
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import "../../App.css";
 
 export const Product = (props) => {
      const {id, productName, price} = props.data;
-    return <div>{productName}</div>;
-    };
+    return (
+          <div className='market'>
+               <img />
+               <div>
+                    <p>{productName}</p>
+                    <p>{price}</p>
+               </div>
+          </div>
+    )
+};
 
